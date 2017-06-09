52b668fdfc08928e759129fa128b2e6b06e9537f
ygout <yohann.goutaret@gmail.com>
2017-06-09 16:13:25 +0200

    Test3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Un deux trois Quatre
+                    Un Deux
                 </div>
 
                 <div class="links">
